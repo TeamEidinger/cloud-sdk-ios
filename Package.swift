7036a4672a8fabf13f7947de019221a4606c3d07
Marco Eidinger <eidingermarco@gmail.com>
2022-04-12 11:33:51 -0700

    chore: vent binary frameworks (requires auth)

--- Package.swift
+++ Package.swift
@@ -2,20 +2,64 @@
 
 import PackageDescription
 
+let version = "7.0.2"
+let root = "https://rbsc.repositories.cloud.sap/nexus3/repository/maven73555000100900005862/com/sap/mobile/platform/client/hcp/sdk/ios"
+
+let sapCommonChecksum = "8615ca61005bd0e92d6a7288d3dc758a6fff3496f9a226eb88d9199ad9cdc004"
+let sapFioriChecksum = "2e800ebb9f1526c2b5c6161b8060af90c1bb50c0cbaa4842dcd48758ec1430e6"
+let sapFioriFlowsChecksum = "6926fecd2d8db906479307aa8f3d0ef06ce00573045ccdf2c35d94d632521690"
+let sapFoundationChecksum = "2c1aec6a97ee9a0286c2153d2c8806ac46b4030a287c85a4b238edc6f9b57fa5"
+let sapODataChecksum = "bd15e46a6c1ab93a09b6825eaee291f250b14479b833dca94332a80bf633e44c"
+let sapOfflineODataChecksum = "53d12728c86be8e417415df060191a122b2e53873d48f843f727436b46e222fb"
+let sapMLChecksum = "d8ed1a8b217111db6a6e35717f8f72d8f96bfe218bef2f74a5132d40f8a30722"
+
 let package = Package(
     name: "cloud-sdk-ios",
-    platforms: [.iOS(.v13)],
-    products: [
-        .library(name: "SAPCommon", targets: ["SAPCommon"]),
-        .library(name: "SAPFoundation", targets: ["SAPFoundation"]),
+    platforms: [
+        .iOS(.v14)
     ],
-    dependencies: [],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "SAPCommon",
+            targets: ["SAPCommon"]),
+        .library(
+            name: "SAPFiori",
+            targets: ["SAPFiori"]),
+        .library(
+            name: "SAPFioriFlows",
+            targets: ["SAPFioriFlows"]),
+        .library(
+            name: "SAPFoundation",
+            targets: ["SAPFoundation"]),
+        .library(
+            name: "SAPOData",
+            targets: ["SAPOData"]),
+        .library(
+            name: "SAPOfflineOData",
+            targets: ["SAPOfflineOData"]),
+        .library(
+            name: "SAPML",
+            targets: ["SAPML"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
     targets: [
-        .binaryTarget(name: "SAPCommon",
-                      url: "https://github.com/SAP/cloud-sdk-ios/releases/download/6.1.2-xcfrwk/SAPCommon-6.1.2-Release-xcframework.zip",
-                      checksum: "bb813a468750f08bdb3f3e49a81e68edb17b6745dcd7f980ee9a87b0f376e959"),
-        .binaryTarget(name: "SAPFoundation",
-                      url: "https://github.com/SAP/cloud-sdk-ios/releases/download/6.1.2-xcfrwk/SAPFoundation-6.1.2-Release-xcframework.zip",
-                      checksum: "21f5bc7d9055743c82e33b82834071b764f3955aaab288aa5f03236948a28956"),
+        .binaryTarget(name: "SAPCommon", url: "\(root)/foundation/SAPCommon/\(version)/SAPCommon-\(version)-Release-xcframework.zip",
+                      checksum: sapCommonChecksum),
+        .binaryTarget(name: "SAPFiori", url: "\(root)/uireuse/SAPFiori/\(version)/SAPFiori-\(version)-Release-xcframework.zip",
+                      checksum: sapFioriChecksum),
+        .binaryTarget(name: "SAPFioriFlows", url: "\(root)/flows/SAPFioriFlows/\(version)/SAPFioriFlows-\(version)-Release-xcframework.zip",
+                      checksum: sapFioriFlowsChecksum),
+        .binaryTarget(name: "SAPFoundation", url: "\(root)/foundation/SAPFoundation/\(version)/SAPFoundation-\(version)-Release-xcframework.zip",
+                      checksum: sapFoundationChecksum),
+        .binaryTarget(name: "SAPOData", url: "\(root)/foundation/SAPOData/\(version)/SAPOData-\(version)-Release-xcframework.zip",
+                      checksum: sapODataChecksum),
+        .binaryTarget(name: "SAPOfflineOData", url: "\(root)/foundation/SAPOfflineOData/\(version)/SAPOfflineOData-\(version)-Release-xcframework.zip",
+                      checksum: sapOfflineODataChecksum),
+        .binaryTarget(name: "SAPML", url: "\(root)/uireuse/SAPML/\(version)/SAPML-\(version)-Release-xcframework.zip",
+                      checksum: sapMLChecksum)
     ]
 )
